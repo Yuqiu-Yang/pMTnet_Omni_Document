40d2b27cfeacd9c463e03a50c126d16d9d5f9aef
“Yuqiu <yyqengels@gmail.com>
2023-03-02 18:35:07 -0600

    update version info

--- pMTnet_Omni_Document/__init__.py
+++ pMTnet_Omni_Document/__init__.py
@@ -4,5 +4,5 @@ pMTnet Omni Document
 Document for pMTnet Omni
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Yi Han, Yuqiu Yang, Tao Wang"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import shutil
 import distutils.cmd
 
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 class PypiCommand(distutils.cmd.Command):
     
